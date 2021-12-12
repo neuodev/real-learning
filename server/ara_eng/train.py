d2a30510bbe351fd9239110fce65ccec38597312
Ahmed Ibrahim <ib153507@gmail.com>
2021-12-12 13:30:05 +0200

    Tokenize the language words

--- server/ara_eng/train.py
+++ server/ara_eng/train.py
@@ -8,7 +8,7 @@ from tensorflow.keras.utils import to_categorical
 import string
 import os
 import sys
-import re
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 def load_doc(path):
     with open(path, mode='rt', encoding='utf-8') as f:
@@ -62,15 +62,61 @@ def print_sample(pairs, n_samples):
         f.writelines('\n'.join(lines))
     print(f'Open {file_name}')
 
-DATASET_FILE = './ara.txt'
+def save_pairs(pairs, file):
+    np.savez_compressed(file, pairs)
+    print(f"Open {file}")
+
+def load_cleaned_dataset(file, n_samples):
+    raw_data = np.load(file)['arr_0']
+    print("Dataset: ", raw_data.shape)
+
+    dataset = raw_data[:n_samples, :]
+    np.random.shuffle(dataset)
+    train_size = int(n_samples * 0.9)
+    train = dataset[:train_size, :] 
+    test = dataset[train_size:, :]
+    return dataset, train, test
+    
+def create_tokenizer(lines):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+def max_length(lines):
+	return max(len(line.split()) for line in lines)
+
+DATASET_FILE = 'ara.txt'
+CLEANED_FILE = 'cleaned.npz'
+n_sentences = 10000
+
 if not os.path.exists(DATASET_FILE):
     print(f"'{DATASET_FILE}' doesn't exist.")
     sys.exit(1)
 
-doc = load_doc(DATASET_FILE)
-pairs = to_pair(doc)
-cleaned = clean_pairs(pairs)
-cleaned = remove_empty_pairs(cleaned)
+
+dataset, train, test = load_cleaned_dataset(CLEANED_FILE, n_sentences)
+print("Whole Dataset: ", dataset.shape)
+print("Traning Dataset: ", train.shape)
+print("Testing Dataset: ", test.shape)
+eng_tokenizer = create_tokenizer(dataset[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1 # +1 for unknow words
+eng_length = max_length(dataset[:, 0])
+print("English Vocab. Size: %s" % (eng_vocab_size))
+print("Max length for english sentences: %s" % (eng_length))
+ara_tokenizer = create_tokenizer(dataset[:, 1])
+ara_vocab_size = len(ara_tokenizer.word_index) + 1 
+ara_length = max_length(dataset[:, 1])
+print("Arabic Vocab. Size: %s" % (ara_vocab_size))
+print("Max length for Arabic sentences: %s" % (ara_length))
+
+
+# Params
+if '--save-clean' in sys.argv:
+    doc = load_doc(DATASET_FILE)
+    pairs = to_pair(doc)
+    cleaned = clean_pairs(pairs)
+    cleaned = remove_empty_pairs(cleaned)
+    save_pairs(cleaned, CLEANED_FILE)
 
 if '--sample' in sys.argv:
     print_sample(cleaned, 1000)
